c527dfe83f9dfbf1696a2bc8ccbbb4876916c08f
Jenny <jenniferrbian@gmail.com>
2026-01-22 16:29:10 -0500

    Added critters to titlepage!! <3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame, sys
+import pygame, sys
+import math
+tilt_angle = 0
 #initialization
 pygame.init()
 
@@ -11,7 +14,7 @@ welcomeBG = pygame.image.load('assets/welcomeBG.png').convert_alpha()
 welcomeBG = pygame.transform.scale(welcomeBG, (800, 800))
 
 #window name
-pygame.display.set_caption('Paws and Pixels')
+pygame.display.set_caption('Paws and Pivots')
 
 #font
 font = pygame.font.SysFont('arial', 30)
@@ -39,6 +42,11 @@ class Button():
         self.grown_rect = self.grown_image.get_rect(center = (self.x_pos, self.y_pos))
         self.original_rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
         self.rect = self.original_rect
+
+    # tilt animation
+    def tilt(self, angle):
+        self.image = pygame.transform.rotate(self.original_image, angle)
+        self.rect = self.image.get_rect(center=self.original_rect.center)
 
 
 
@@ -69,13 +77,56 @@ playbutton = Button(210, 625, playbutton_surface)
 
 gobutton_surface = pygame.image.load('assets/gobutton.png').convert_alpha()
 gobutton = Button(300, 450, gobutton_surface)
-def main_menu():
 
+#critters loading and resizing
+critter_scale = 0.6  # 60% of original size
+
+critter_sufrace1 = pygame.image.load('assets/Subject.png').convert_alpha()
+critter_sufrace1 = pygame.transform.scale(
+    critter_sufrace1,
+    (int(critter_sufrace1.get_width() * critter_scale),
+     int(critter_sufrace1.get_height() * critter_scale))
+)
+
+critter_sufrace2 = pygame.image.load('assets/Subject 2.png').convert_alpha()
+critter_sufrace2 = pygame.transform.scale(
+    critter_sufrace2,
+    (int(critter_sufrace2.get_width() * critter_scale),
+     int(critter_sufrace2.get_height() * critter_scale))
+)
+
+critter_sufrace3 = pygame.image.load('assets/Subject 3.png').convert_alpha()
+critter_sufrace3 = pygame.transform.scale(
+    critter_sufrace3,
+    (int(critter_sufrace3.get_width() * critter_scale),
+     int(critter_sufrace3.get_height() * critter_scale))
+)
+
+critter_sufrace4 = pygame.image.load('assets/Subject 4.png').convert_alpha()
+critter_sufrace4 = pygame.transform.scale(
+    critter_sufrace4,
+    (int(critter_sufrace4.get_width() * critter_scale),
+     int(critter_sufrace4.get_height() * critter_scale))
+)
+
+critter1 = Button(250, 80, critter_sufrace1)  # dubai
+critter2 = Button(650, 45, critter_sufrace2)  # monkey
+critter3 = Button(150, 400, critter_sufrace3)  # bunny
+critter4 = Button(550, 625, critter_sufrace4)  # kitty
+
+
+def main_menu():
+    global tilt_angle
     while True:
         SCREEN.blit(BG, (0, 0))
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-
+        tilt_angle += 0.025
+        tilt = math.sin(tilt_angle) * 10
+        critter1.tilt(tilt)
+        critter2.tilt(tilt)
+        critter3.tilt(tilt)
+        critter4.tilt(tilt)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -89,6 +140,11 @@ def main_menu():
         playbutton.grow(playbutton.checkForInput(MENU_MOUSE_POS))
         playbutton.changeCursor(playbutton.checkForInput(MENU_MOUSE_POS))
         playbutton.update()
+
+        critter1.update()
+        critter2.update()
+        critter3.update()
+        critter4.update()
 
         pygame.display.update()
 
